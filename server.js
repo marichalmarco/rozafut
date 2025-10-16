0665f9ae6b8af65c90c2004bc59301e2c2db2685
Marco Marichal <mmaricha@cr.ibm.com>
2025-10-16 17:09:07 -0600

    Change for Testing

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // ROUTES
 app.get("/rosafut", (req, res) => res.render("rosafut"));
-app.get("/", (req, res) => res.send("home"));
+app.get("/", (req, res) => res.render("home"));
 
 // ... all your other routes remain exactly the same ...
 
